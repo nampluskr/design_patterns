b05fb3dc358b982dda53c5509ca78f051f6f2ae4
nampluskr <37031766+nampluskr@users.noreply.github.com>
2024-12-18 19:43:45 +0900

    Update quack_behavoir.h

--- head_first_cpp/chap-01_strategy/quack_behavoir.h
+++ head_first_cpp/chap-01_strategy/quack_behavoir.h
@@ -6,23 +6,17 @@ using namespace std;
 // Interface
 struct QuackBehavoir {
     virtual ~QuackBehavoir() = default;
-    virtual void quack() const = 0;
+    virtual void quack() = 0;
 };
 
 struct Quack : QuackBehavoir {
-    void quack() const override {
-        cout << "Quack." << endl;
-    }
+    void quack() override { cout << "Quack." << endl; }
 };
 
 struct MuteQuack : QuackBehavoir {
-    void quack() const override {
-        cout << "<< Silence >>" << endl;
-    }
+    void quack() override { cout << "<< Silence >>" << endl; }
 };
 
 struct Squeak : QuackBehavoir {
-    void quack() const override {
-        cout << "Squeak." << endl;
-    }
+    void quack() override { cout << "Squeak." << endl; }
 };
