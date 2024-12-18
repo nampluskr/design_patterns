c4226582ce1d376ed37715600e22ac0de1ed97d5
nampluskr <37031766+nampluskr@users.noreply.github.com>
2024-12-18 19:43:30 +0900

    Update fly_behavoir.h

--- head_first_cpp/chap-01_strategy/fly_behavoir.h
+++ head_first_cpp/chap-01_strategy/fly_behavoir.h
@@ -6,23 +6,17 @@ using namespace std;
 // Interface
 struct FlyBehavoir {
     virtual ~FlyBehavoir() = default;
-    virtual void fly() const = 0;
+    virtual void fly() = 0;
 };
 
 struct FlyWithWings : FlyBehavoir {
-    void fly() const override {
-        cout << "I'm flying!!" << endl;
-    }
+    void fly() override { cout << "I'm flying!!" << endl; }
 };
 
-struct FlyNoWay : FlyBehavoir {
-    void fly() const override {
-        cout << "I can't fly." << endl;
-    }
+struct FlyNoWay : FlyBehavoir { 
+    void fly() override { cout << "I can't fly." << endl; }
 };
 
 struct FlyRocketPowered : FlyBehavoir {
-    void fly() const override {
-        cout << "I'm flying with a roket!" << endl;
-    }
+    void fly() override { cout << "I'm flying with a roket!" << endl; }
 };
